ceebc294b67e750d7bacc4d06299f9156178f955
Tyler D <tyler@tdaigle.com>
2023-05-15 14:30:51 -0400

    added prisma - generated schema

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `issues` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(200) NOT NULL,
+    `tech_used` VARCHAR(200) NULL,
+    `difficulty_level` VARCHAR(15) NULL,
+    `issue_number` INTEGER NOT NULL,
+    `date_created` DATE NULL,
+    `issue_description` TEXT NULL,
+    `issue_link` VARCHAR(255) NULL,
+    `repo_link` VARCHAR(255) NULL,
+    `repo_name` VARCHAR(255) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
