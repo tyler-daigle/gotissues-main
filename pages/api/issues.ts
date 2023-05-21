70e86d70c67dc5d3ead4b7601cb311062128696e
Tyler D <tyler@tdaigle.com>
2023-05-21 15:30:39 -0400

    add issues endpoint working - added 'none' difficulty tag for unset values

--- pages/api/issues.ts
+++ pages/api/issues.ts
@@ -11,27 +11,36 @@ export default async function handler(
   if (req.method !== "GET") {
     res.status(404).send("<h1>404 - Not Found</h1>");
   }
+  const pageSize = 10;
+  const totalIssues = await prisma.issues.count();
+  let totalPages = Math.ceil(totalIssues / pageSize);
+  console.log(
+    `${totalIssues} issues in the database - total Pages ${totalPages}`
+  );
   try {
     const { page, limit } = req.query;
-
-    const pageSize = 10;
 
     // check the params that are passed
     let pageNum = page && typeof page === "string" ? Number.parseInt(page) : 0;
     let resultsLimit =
       limit && typeof limit === "string" ? Number.parseInt(limit) : 10;
 
+    console.log(`Getting page ${pageNum}`);
     const issuesArray: IssueType[] = [];
     // const issueItem = await prisma.issues.findFirstOrThrow();
-    const issuesItems = await prisma.issues.findMany({ take: resultsLimit });
+    const issuesItems = await prisma.issues.findMany({
+      skip: pageNum * pageSize,
+      take: resultsLimit,
+    });
+
     issuesArray.push(...issuesItems);
     console.log("Issues Endpoint hit...");
 
     res
       .status(200)
-      .json({ issues: issuesArray, count: issuesArray.length, totalPages: 2 });
+      .json({ issues: issuesArray, count: issuesArray.length, totalPages });
   } catch (error: any) {
     console.error(error.message);
-    res.status(200).json({ issues: [], count: 0, totalPages: 0 }); //TODO: what is the proper way to handle a bad request?
+    res.status(200).json({ issues: [], count: 0, totalPages }); //TODO: what is the proper way to handle a bad request?
   }
 }
--- pages/components/DifficultyTag.tsx
+++ pages/components/DifficultyTag.tsx
@@ -1,10 +1,18 @@
 import styles from "@/styles/DifficultyTag.module.css";
 
 interface Props {
-  difficultyLevel: "advanced" | "beginner" | "intermediate";
+  difficultyLevel: "advanced" | "beginner" | "intermediate" | "none";
 }
 
 export default function DifficultyTag({ difficultyLevel }: Props) {
+  if (
+    difficultyLevel !== "advanced" &&
+    difficultyLevel !== "beginner" &&
+    difficultyLevel !== "intermediate"
+  ) {
+    difficultyLevel = "none";
+  }
+
   let classNames = `${styles.difficultyTag} ${styles[difficultyLevel]}`;
   return (
     <span className={classNames} title={difficultyLevel}>
